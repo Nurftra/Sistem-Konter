1d1f1e084f221860adef10cb06900fdb2bd7f4b5
Farel1237 <farelgoo32@gmail.com>
2025-12-11 02:50:01 +0800

    fix

--- run.py
+++ run.py
@@ -6,10 +6,14 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from app import create_app
+from app import create_app, db
 import app.cli
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+    print("Tabel berhasil dibuat!")
 
 if __name__ == '__main__':
     # Use environment variable for debug mode, default to False if not set
